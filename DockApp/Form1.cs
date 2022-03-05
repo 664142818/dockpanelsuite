d57f476dc5bd33071b6599c15d93f5b751b01e7c
zhanleiwang@trip.com <zhanleiwang@trip.com>
2022-03-05 21:44:16 +0800

    Form2

--- DockApp/Form1.cs
+++ DockApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace DockApp
 {
@@ -15,6 +16,13 @@ namespace DockApp
         public Form1()
         {
             InitializeComponent();
+            this.IsMdiContainer = true; 
+             Form2 f2 = new Form2();
+            f2.Show(dockPanel1, DockState.DockLeft);
+            Form3 f3 = new Form3();
+            f3.Show(dockPanel1, DockState.DockRight);
+            Form4 f4 = new Form4();
+            f4.Show(dockPanel1, DockState.Document);
         }
     }
 }
